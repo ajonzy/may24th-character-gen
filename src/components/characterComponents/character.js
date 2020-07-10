f9112aad5dbdda8477b68161b2a361cf5e781d6d
Alex Jones <ajonzy@gmail.com>
2020-07-10 10:08:11 -0600

    Added character options

--- src/components/characterComponents/character.js
+++ src/components/characterComponents/character.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import Description from "./description"
+import Options from "./options"
 
 export default class Character extends Component {
    constructor(props) {
@@ -12,11 +13,41 @@ export default class Character extends Component {
            hitpoints: 100,
            lastAction: ""
        }
+
+       this.handleFight = this.handleFight.bind(this)
+       this.handleEat = this.handleEat.bind(this)
+       this.handleRest = this.handleRest.bind(this)
    }
+
+   handleFight() {
+       this.setState({ 
+           hitpoints: this.state.hitpoints - 10,
+           lastAction: "Fight"
+        })
+   }
+
+   handleEat() {
+        this.setState({ 
+            hitpoints: this.state.hitpoints + 10,
+            lastAction: "Eat"
+        })
+    }
+
+    handleRest() {
+        this.setState({ 
+            hitpoints: 100,
+            lastAction: "Rest"
+         })
+    }
 
    render() {
        return (
            <div className='character-wrapper'>
+                <Options
+                    handleFight={this.handleFight}
+                    handleEat={this.handleEat}
+                    handleRest={this.handleRest}
+                />
                <Description 
                     name={this.state.name}
                     characterClass={this.state.characterClass}
